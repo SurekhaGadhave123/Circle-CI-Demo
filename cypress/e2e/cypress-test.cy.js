d6eb70ac9009a5e51b8c292f7e71929db40a6f84
SurekhaGadhave123 <surekha@simformsolutions.com>
2023-10-02 13:53:38 +0530

    first commit

---
+++ cypress/e2e/cypress-test.cy.js
@@ -0,0 +1,6 @@
+describe("practice code",()=>{
+    it("user should be able to print data",()=>{
+        cy.visit("https://www.google.com/")
+        cy.visit("https://www.microsoft.com/en-in/")
+    })
+})
